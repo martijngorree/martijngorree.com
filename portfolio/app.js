d923a5d0b75594810707daa1ad1f43fb509625cf
Martijn Gorree <martijng@fabrique.nl>
2016-02-18 16:25:20 +0100

    Work in progress, setting up backend integration

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,34 +1,57 @@
 "use strict";
-import express from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 
-import routes from './routes/index';
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _serveFavicon = require('serve-favicon');
+
+var _serveFavicon2 = _interopRequireDefault(_serveFavicon);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _cookieParser = require('cookie-parser');
+
+var _cookieParser2 = _interopRequireDefault(_cookieParser);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _index = require('./routes/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 //import users from './routes/users';
 
-let app = express();
+var app = (0, _express2.default)();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', _path2.default.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((0, _morgan2.default)('dev'));
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use((0, _cookieParser2.default)());
+app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', _index2.default);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-    let err = new Error('Not Found');
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
@@ -38,7 +61,7 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -49,7 +72,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
