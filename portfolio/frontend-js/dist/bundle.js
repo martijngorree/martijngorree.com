950d28e745772eab0617b0219b55f6f5ff684335
Martijn Gorree <martijng@fabrique.nl>
2016-03-10 16:03:21 +0100

    Fixed embeddable links in answers

--- portfolio/frontend-js/dist/bundle.js
+++ portfolio/frontend-js/dist/bundle.js
@@ -10461,12 +10461,12 @@
 
 	var Handlebars = __webpack_require__(9);
 	module.exports = (Handlebars["default"] || Handlebars).template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-	    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+	    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function";
 
 	  return "<div class=\"exp-bingo__answer__title\">"
-	    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-	    + "</div>\n<div class=\"exp-bingo__answer__descr\">"
-	    + alias4(((helper = (helper = helpers.descr || (depth0 != null ? depth0.descr : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"descr","hash":{},"data":data}) : helper)))
+	    + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+	    + "</div>\n<div class=\"exp-bingo__answer__descr\">!"
+	    + ((stack1 = ((helper = (helper = helpers.descr || (depth0 != null ? depth0.descr : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"descr","hash":{},"data":data}) : helper))) != null ? stack1 : "")
 	    + "</div>\n";
 	},"useData":true});
 
